1bbf5c16b2805ee3ba79197077cf338995f26e5c
Mr. Fast <33694976+HerrSchnell@users.noreply.github.com>
2024-02-14 08:28:37 -0800

    Initial commit

---
+++ 4-dominant-writing-direction.js
@@ -0,0 +1,10 @@
+require('./includes/scripts.js') // include the scripts used by the chapter
+
+function dominantDirection(text) {
+    // Your code here.
+  }
+  
+  console.log(dominantDirection("Hello!"));
+  // → ltr
+  console.log(dominantDirection("Hey, مساء الخير"));
+  // → rtl
